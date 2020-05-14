32c08016253ca6baf0f18097d20289b2a5abd152
liyi <apa777@yeah.net>
2020-05-14 10:05:06 +0800

    leetcode 剑指offer4补充

--- leetcode/src/main/java/com/leetcode/jzoffer/Problem4.java
+++ leetcode/src/main/java/com/leetcode/jzoffer/Problem4.java
@@ -56,6 +56,11 @@ public class Problem4 {
      * 大于targer则索引-1
      * 小于targer则索引+1
      *
+     * 选左上角，往右走和往下走都增大，不能选
+     * 选右下角，往上走和往左走都减小，不能选
+     * 选左下角，往右走增大，往上走减小，可选
+     * 选右上角，往下走增大，往左走减小，可选
+     *
      * @param matrix
      * @param target
      * @return
