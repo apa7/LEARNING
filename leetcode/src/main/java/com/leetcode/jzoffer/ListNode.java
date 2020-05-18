f4329178e72406e323c76436a20a5053b16dafee
liyi <apa777@yeah.net>
2020-05-18 13:35:07 +0800

    删除链表的节点

--- leetcode/src/main/java/com/leetcode/jzoffer/ListNode.java
+++ leetcode/src/main/java/com/leetcode/jzoffer/ListNode.java
@@ -7,4 +7,9 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
+    public ListNode setNext(int x) {
+        this.next = new ListNode(x);
+        return this.next;
+    }
 }
