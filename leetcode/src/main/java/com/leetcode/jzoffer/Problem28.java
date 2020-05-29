d9e71476b4700b382468d788466f7916ddf3ac2b
apa7 <1056904195@qq.com>
2020-05-29 18:54:15 +0800

    面试题28. 对称的二叉树

---
+++ leetcode/src/main/java/com/leetcode/jzoffer/Problem28.java
@@ -0,0 +1,69 @@
+package com.leetcode.jzoffer;
+
+/**
+ * 面试题28. 对称的二叉树
+ * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ * <p>
+ * <p>
+ * 限制：
+ * <p>
+ * 0 <= 节点个数 <= 1000
+ * <p>
+ * 注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/
+ * Created by apa7 on 2020/5/29.
+ */
+public class Problem28 {
+
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null) {
+            return right == null;
+        }
+        if (right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
+    public static void main(String[] args) {
+        Problem28 p = new Problem28();
+        TreeNode root = new TreeNode(1);
+        root.setLR(2, 2);
+        root.left.setLR(null, 4);
+        root.right.setLR(null, 4);
+        boolean r = p.isSymmetric(root);
+        System.out.println(r);
+    }
+}
