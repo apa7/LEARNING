c97a7b27de3154885cec5d55ba863d65d0ece3fc
apa7 <1056904195@qq.com>
2020-06-27 20:56:58 +0800

    33. 搜索旋转排序数组

---
+++ leetcode/src/main/java/com/leetcode/problems/Problem33.java
@@ -0,0 +1,65 @@
+package com.leetcode.problems;
+
+import java.util.Arrays;
+
+/**
+ * 33. 搜索旋转排序数组
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ * <p>
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ * <p>
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ * <p>
+ * 你可以假设数组中不存在重复的元素。
+ * <p>
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [4,5,6,7,0,1,2], target = 0
+ * 输出: 4
+ * 示例 2:
+ * <p>
+ * 输入: nums = [4,5,6,7,0,1,2], target = 3
+ * 输出: -1
+ * Created by apa7 on 2020/6/27.
+ */
+public class Problem33 {
+
+    //如果中间的数小于最右边的数，则右半段是有序的，若中间数大于最右边数，则左半段是有序的，我们只要在有序的半段里用首尾两个数组来判断目标值是否在这一区域内，这样就可以确定保留哪半边了
+    public int search(int[] nums, int target) {
+        if (nums.length == 0 || (nums.length == 1 && nums[0] != target)) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < nums[right]) {
+                //右半段有序
+                if (nums[mid] < target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                //左半段有序
+                if (nums[mid] > target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        Problem33 p = new Problem33();
+        int r = p.search(arr, 0);
+        System.out.println(r);
+    }
+}
