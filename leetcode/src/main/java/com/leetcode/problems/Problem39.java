96fa48bd6994e8453593ccc79f11e182ec211be7
apa7 <1056904195@qq.com>
2020-07-01 22:25:07 +0800

    39. 组合总和

---
+++ leetcode/src/main/java/com/leetcode/problems/Problem39.java
@@ -0,0 +1,48 @@
+package com.leetcode.problems;
+
+import java.util.List;
+
+/**
+ * 39. 组合总和
+ * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ * <p>
+ * candidates 中的数字可以无限制重复被选取。
+ * <p>
+ * 说明：
+ * <p>
+ * 所有数字（包括 target）都是正整数。
+ * 解集不能包含重复的组合。
+ * 示例 1:
+ * <p>
+ * 输入: candidates = [2,3,6,7], target = 7,
+ * 所求解集为:
+ * [
+ * [7],
+ * [2,2,3]
+ * ]
+ * 示例 2:
+ * <p>
+ * 输入: candidates = [2,3,5], target = 8,
+ * 所求解集为:
+ * [
+ * [2,2,2,2],
+ * [2,3,3],
+ * [3,5]
+ * ]
+ * Created by apa7 on 2020/6/30.
+ */
+public class Problem39 {
+
+    /*public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        int len = candidates.length;
+        int[][] arr = new int[len][len];
+        for (int i = 0; i < target; i++) {
+            for (int candidate : candidates) {
+
+            }
+        }
+//        arr[len - 1][];
+    }*/
+
+
+}
